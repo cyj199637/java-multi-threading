6904b0e30657c221fd97e0d933c18638c5957362
yoonjeong <cyj199637@gmail.com>
2023-04-17 01:30:11 +0900

    section2. code practice 1: create thread - MultiExecutor

---
+++ create-thread/MultiExecutor.java
@@ -0,0 +1,23 @@
+import java.util.List;
+
+public class MultiExecutor {
+
+    private final List<Runnable> tasks;
+
+    /*
+     * @param tasks to executed concurrently
+     */
+    public MultiExecutor(List<Runnable> tasks) {
+        this.tasks = tasks;
+    }
+
+    /**
+     * Starts and executes all the tasks concurrently
+     */
+    public void executeAll() {
+        for (Runnable task : tasks) {
+            Thread thread = new Thread(task);
+            thread.start();
+        }
+    }
+}
