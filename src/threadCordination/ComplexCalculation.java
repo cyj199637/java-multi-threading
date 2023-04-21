1a84f22c77268d00c3c2469434c685f3f12de767
yoonjeong <cyj199637@gmail.com>
2023-04-22 02:01:55 +0900

    section3. code practice 2: multithreading calculation

---
+++ src/threadCordination/ComplexCalculation.java
@@ -0,0 +1,47 @@
+package threadCordination;
+
+import java.math.BigInteger;
+
+public class ComplexCalculation {
+    public BigInteger calculateResult(BigInteger base1, BigInteger power1, BigInteger base2, BigInteger power2) {
+        BigInteger result;
+
+        PowerCalculatingThread thread1 = new PowerCalculatingThread(base1, power1);
+        PowerCalculatingThread thread2 = new PowerCalculatingThread(base2, power2);
+
+        thread1.start();
+        thread2.start();
+
+        try {
+            thread1.join();
+            thread2.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        BigInteger tempResult1 = thread1.getResult();
+        BigInteger tempResult2 = thread2.getResult();
+
+        result = tempResult1.add(tempResult2);
+
+        return result;
+    }
+
+    private static class PowerCalculatingThread extends Thread {
+        private BigInteger result = BigInteger.ONE;
+        private BigInteger base;
+        private BigInteger power;
+
+        public PowerCalculatingThread(BigInteger base, BigInteger power) {
+            this.base = base;
+            this.power = power;
+        }
+
+        @Override
+        public void run() {
+            this.result = this.base.pow(this.power.intValue());
+        }
+
+        public BigInteger getResult() { return result; }
+    }
+}
